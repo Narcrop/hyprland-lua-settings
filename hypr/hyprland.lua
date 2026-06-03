ead78382eadea7fe714ef5618ede9b9e319a627d
Narcrop <90524289+Narcrop@users.noreply.github.com>
2026-06-03 13:48:02 -0500

    Add files via upload

---
+++ hypr/hyprland.lua
@@ -0,0 +1,14 @@
+require("modules.monitors")
+require("modules.input")
+require("modules.autostart")
+require("modules.env")
+require("modules.permissions")
+require("modules.lookandfeel")
+require("modules.animations")
+require("modules.windowrules")
+require("modules.layouts")
+require("modules.misc")
+require("modules.binds")
+
+-- For Noctalia Color templates
+require("noctalia")
